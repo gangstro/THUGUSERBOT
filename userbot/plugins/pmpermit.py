2ab91788e0f93e1f5f2f05215f252b101e94f85f
gangstro <71931663+gangstro@users.noreply.github.com>
2020-10-10 22:07:44 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -12,7 +12,7 @@ from userbot import CMD_HELP
 
 PMPERMIT_PIC = os.environ.get("PMPERMIT_PIC", None)
 if PMPERMIT_PIC is None:
-  WARN_PIC = "https://telegra.ph/file/db92ed3d77377856ef911.mp4"
+  WARN_PIC = "https://telegra.ph/file/9d61d1f2d73019a1230e9.jpg"
 else:
   WARN_PIC = PMPERMIT_PIC
 
